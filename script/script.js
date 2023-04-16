4a221f584b8bcc57edd76118fd314a2f269848f1
Anvinalias <anvinkaippillil123@gmail.com>
2023-04-16 20:51:30 +0530

    Add a function getComputerChoice
    To let the computer choose a random choice from the options.

---
+++ script/script.js
@@ -0,0 +1,18 @@
+        // let computer choose randomly
+        function getComputerChoice()
+            {
+                console.log("Computer choose:")
+             let randomChoice = Math.floor(Math.random()*3)+1;
+                if (randomChoice===1)
+                {   
+                    return ROCK;
+                }
+                else if(randomChoice===2)
+                {
+                    return PAPER;
+                }
+                else
+                {   
+                    return SCISSORS;
+                }
+            }
